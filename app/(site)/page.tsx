4b5140df2c7a153ec80d881b83a4dda2363f3cd7
garamflow <garamflow@gmail.com>
2024-07-22 00:06:19 +0900

    feat: 더미데이터를 이용한 캐러셀 구현

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,26 +2,50 @@ import Category from "@/app/(site)/components/Category";
 import PagePadding from "@/components/PagePadding";
 import PlaylistCarousel from "@/components/PlaylistCarousel";
 import UserIcon from "@/components/UserIcon";
-import { dummyPlaylistArray } from "@/lib/dummyData";
+import { dummyPlaylistArray, getPlaylistById } from "@/lib/dummyData";
 import React from "react";
 
 const page = async () => {
 	const dummyPlaylistArray1 = [...dummyPlaylistArray];
+	const dummyPlaylistArray2 = [await getPlaylistById(1)];
+	const dummyPlaylistArray3 = [await getPlaylistById(2)];
+	const dummyPlaylistArray4 = [await getPlaylistById(3)];
 
 	return (
 		<PagePadding>
 			<div className='min-h-[600px]'>
 				<div className='mt-9'></div>
 				<Category />
-				<div className='mt-12'></div>
+				<div className='mt-20'></div>
 				<PlaylistCarousel
-					playlist={[...dummyPlaylistArray1]}
+					playlistArray={[...dummyPlaylistArray1]}
 					Thumbnail={
 						<div className='w-[56px] h-[56px]'>
 							<UserIcon size={"lg"} />
 						</div>
 					}
 					title='Replay'
+					subTitle='Youtube'
+				/>
+				<div className='mt-20'></div>
+
+				<PlaylistCarousel
+					playlistArray={[...dummyPlaylistArray1]}
+					title='IU'
+					subTitle='New Album'
+				/>
+				<div className='mt-20'></div>
+
+				<PlaylistCarousel
+					playlistArray={[...dummyPlaylistArray1]}
+					title='Community'
+					subTitle='Youtube'
+				/>
+				<div className='mt-20'></div>
+
+				<PlaylistCarousel
+					playlistArray={[...dummyPlaylistArray1]}
+					title='Cover & Remix'
 					subTitle='Youtube'
 				/>
 			</div>
