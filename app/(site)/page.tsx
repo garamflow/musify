a860ffd4a743fb2d1d1b63e0912fbc4cb405d5e1
garamflow <garamflow@gmail.com>
2024-07-21 21:20:47 +0900

    feat: 홈 카테고리 컴포넌트 구현 및 스타일링

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,16 @@
+import Category from "@/app/(site)/components/Category";
 import { sleep } from "@/lib/utils";
 import React from "react";
 
 const page = async () => {
 	await sleep(2000);
-	return <div>HomePage</div>;
+	return (
+		<div className='min-h-[600px]'>
+			<div className='mt-9'>
+				<Category />
+			</div>
+		</div>
+	);
 };
 
 export default page;
