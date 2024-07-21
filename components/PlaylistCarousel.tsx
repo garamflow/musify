a3c5e5567db42e72dba3ecab0218e42dc43af581
garamflow <garamflow@gmail.com>
2024-07-21 22:31:55 +0900

    feat: shadcn의 Carousel을 이용해 캐러셀 구현 및 스타일링

--- components/PlaylistCarousel.tsx
+++ components/PlaylistCarousel.tsx
@@ -1,5 +1,6 @@
 import { Playlist } from "@/types";
 import React from "react";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 
 interface PlaylistCarouselProps {
 	title?: string;
@@ -10,11 +11,32 @@ interface PlaylistCarouselProps {
 
 const PlaylistCarousel = ({ playlist, Thumbnail, title, subTitle }: PlaylistCarouselProps) => {
 	return (
-		<div>
-			{""}
-			{Thumbnail}
-			{title} - {subTitle}
-			{JSON.stringify(playlist)}
+		<div className='w-full'>
+			<Carousel>
+				<div className='flex flex-row justify-between items-end my-2'>
+					<article className='flex flex-row gap-3'>
+						{Thumbnail}
+						<div className='flex flex-col justify-center'>{subTitle && <div className='text-neutral-500'>{subTitle}</div>}</div>
+						<div className='text-[34px] font-bold leadding-[34px]'>{title}</div>
+					</article>
+					<div className='relative left-[-45px]'>
+						<div className='absolute bottom-[20px]'>
+							<CarouselPrevious className='right-2' />
+							<CarouselNext className='left-2' />
+						</div>
+					</div>
+				</div>
+				<CarouselContent>
+					{Array.from({ length: 5 }).map((_, index) => (
+						<CarouselItem
+							key={index}
+							className='md:basis-1/2 lg:basis-1/3'
+						>
+							<div className='p-1'>card</div>
+						</CarouselItem>
+					))}
+				</CarouselContent>
+			</Carousel>
 		</div>
 	);
 };
