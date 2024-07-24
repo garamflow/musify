a8c9dd19d2344f2f2706c560919e92f24faa82ea
garamflow <garamflow@gmail.com>
2024-07-25 06:58:48 +0900

    feat: 캐러셀 연결 및 스타일링 추가

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,11 +1,13 @@
+import React from "react";
+import { dummyGenreList, getAllPlaylist, getSongListTop10 } from "@/lib/dummyData";
 import Category from "@/app/explore/components/Catagory";
+import GenreListCarousel from "@/components/GenreListCarousel";
 import PagePadding from "@/components/PagePadding";
 import PlaylistCarousel from "@/components/PlaylistCarousel";
-import { getAllPlaylist } from "@/lib/dummyData";
-import React from "react";
+import SongListCarousel from "@/components/SongListCarousel";
 
 const page = async () => {
-	const playlistArray = await getAllPlaylist();
+	const [playlistArray, songListTop10] = await Promise.all([getAllPlaylist(), getSongListTop10()]);
 
 	return (
 		<PagePadding>
@@ -16,6 +18,15 @@ const page = async () => {
 				playlistArray={playlistArray}
 				title='New Album & Single'
 			/>
+			<div className='mt-20'></div>
+			<SongListCarousel
+				songListTop10={songListTop10}
+				title='Trends'
+			/>
+			<div className='mt-20'></div>
+			<GenreListCarousel genreList={dummyGenreList} />
+			<div className='mt-20'></div>
+			<div className='mt-20'></div>
 		</PagePadding>
 	);
 };
