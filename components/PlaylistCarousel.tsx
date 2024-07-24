9b8ff1d004ec53092553f158ac060965fa7a43b4
garamflow <garamflow@gmail.com>
2024-07-24 11:43:40 +0900

    style: 반응형에 따라 캐러셀 아이템 갯수 조절 추가

--- components/PlaylistCarousel.tsx
+++ components/PlaylistCarousel.tsx
@@ -32,7 +32,7 @@ const PlaylistCarousel = ({ playlistArray, Thumbnail, title, subTitle }: Playlis
 						return (
 							<CarouselItem
 								key={index}
-								className='md:basis-1/2 lg:basis-1/3 xl:basis-1/5'
+								className='basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5'
 							>
 								<PlaylistCard playlist={playlist} />
 							</CarouselItem>
