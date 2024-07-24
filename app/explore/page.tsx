bb92dd001c817b276fc6e17e351044ba46e3a08c
garamflow <garamflow@gmail.com>
2024-07-24 11:44:06 +0900

    feat: 검색 페이지에서 플레이 리스트 캐러셀 추가

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,12 +1,21 @@
 import Category from "@/app/explore/components/Catagory";
 import PagePadding from "@/components/PagePadding";
+import PlaylistCarousel from "@/components/PlaylistCarousel";
+import { getAllPlaylist } from "@/lib/dummyData";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+	const playlistArray = await getAllPlaylist();
+
 	return (
 		<PagePadding>
 			<div className='mt-4'></div>
 			<Category />
+			<div className='mt-20'></div>
+			<PlaylistCarousel
+				playlistArray={playlistArray}
+				title='New Album & Single'
+			/>
 		</PagePadding>
 	);
 };
