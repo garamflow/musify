4f28fb158b53a212ecf03bebf4da2af2fa6e66ae
garamflow <garamflow@gmail.com>
2024-07-27 23:21:30 +0900

    feat: 라이브러리 페이지에 플레이리스트와 카테고리 연결

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import PagePadding from "@/components/PagePadding";
+import Category from "@/app/library/components/Category";
+import PlaylistCard from "@/components/PlaylistCard";
+import { getRandomElementFromArray } from "@/lib/utils";
+import { dummyPlaylistArray } from "@/lib/dummyData";
 
 const page = () => {
-	return <div>library</div>;
+	const randomPlaylist = getRandomElementFromArray(dummyPlaylistArray) || {
+		id: 0,
+		owner: "Unknown",
+		playlistName: "Unknown Playlist",
+		songList: [],
+	};
+
+	return (
+		<PagePadding>
+			<div className='mt-9'></div>
+			<Category />
+			<div className='mt-12'></div>
+			<section className='grid grid-cols-3 gap-6 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+				<PlaylistCard playlist={randomPlaylist} />
+				<PlaylistCard playlist={randomPlaylist} />
+				<PlaylistCard playlist={randomPlaylist} />
+				<PlaylistCard playlist={randomPlaylist} />
+				<PlaylistCard playlist={randomPlaylist} />
+				<PlaylistCard playlist={randomPlaylist} />
+				<PlaylistCard playlist={randomPlaylist} />
+				<PlaylistCard playlist={randomPlaylist} />
+				<PlaylistCard playlist={randomPlaylist} />
+				<PlaylistCard playlist={randomPlaylist} />
+				<PlaylistCard playlist={randomPlaylist} />
+			</section>
+			<div className='mt-20'></div>
+		</PagePadding>
+	);
 };
 
 export default page;
