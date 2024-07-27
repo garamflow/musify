ee59618807439bb24ed0614cfe9ee0cec25d7541
garamflow <garamflow@gmail.com>
2024-07-28 01:55:09 +0900

    feat: 채널 페이지에서 채널정보 없을 시 메인화면으로 이동 및 채널 헤더 구현

--- app/channel/[id]/page.tsx
+++ app/channel/[id]/page.tsx
@@ -1,7 +1,28 @@
+import HeaderBgChanger from "@/components/HeaderBgChanger";
+import PagePadding from "@/components/PagePadding";
+import { getChannelById } from "@/lib/dummyData";
+import { getRandomElementFromArray } from "@/lib/utils";
+import { permanentRedirect } from "next/navigation";
 import React from "react";
 
-const page = () => {
-	return <div>channel</div>;
+interface ChannelPageProps {
+	params: {
+		id: string;
+	};
+}
+
+const page = async (props: ChannelPageProps) => {
+	const channel = await getChannelById(Number(props.params.id));
+
+	if (!channel) permanentRedirect("/");
+
+	const imageSrc = getRandomElementFromArray(channel.songList)?.imageSrc;
+
+	return (
+		<PagePadding>
+			<HeaderBgChanger imageSrc={imageSrc} />
+		</PagePadding>
+	);
 };
 
 export default page;
