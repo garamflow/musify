cc8dac281f437585920a431dcd1c179341a93098
garamflow <garamflow@gmail.com>
2024-07-28 00:29:06 +0900

    feat: 플레이리스트 페이지 헤더와 플레이리스트 헤드부분 추가

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import HeaderBgChanger from "@/components/HeaderBgChanger";
+import { getPlaylistById } from "@/lib/dummyData";
+import { getRandomElementFromArray } from "@/lib/utils";
+import { permanentRedirect } from "next/navigation";
+import PagePadding from "@/components/PagePadding";
+import PlaylistHead from "@/components/PlaylistHead";
 
-const page = () => {
-	return <div>playlist</div>;
+interface PlaylistProps {
+	searchParams: {
+		list: string;
+	};
+}
+
+const page = async (props: PlaylistProps) => {
+	const playlist = await getPlaylistById(Number(props.searchParams.list));
+
+	if (!playlist) permanentRedirect("/");
+
+	const imageSrc = getRandomElementFromArray(playlist.songList)?.imageSrc;
+
+	return (
+		<PagePadding>
+			<HeaderBgChanger imageSrc={imageSrc} />
+			<div className='mt-12'></div>
+			<PlaylistHead playlist={playlist} />
+		</PagePadding>
+	);
 };
 
 export default page;
