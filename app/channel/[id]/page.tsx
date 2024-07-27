6b7de7f49a314bdeec131f54896ea95ca8022cd9
garamflow <garamflow@gmail.com>
2024-07-28 02:24:54 +0900

    feat: 채널 페이지 구현 및 스타일링

--- app/channel/[id]/page.tsx
+++ app/channel/[id]/page.tsx
@@ -1,9 +1,14 @@
-import HeaderBgChanger from "@/components/HeaderBgChanger";
-import PagePadding from "@/components/PagePadding";
+import React from "react";
+import { permanentRedirect } from "next/navigation";
+import { FiMusic, FiShuffle } from "react-icons/fi";
 import { getChannelById } from "@/lib/dummyData";
 import { getRandomElementFromArray } from "@/lib/utils";
-import { permanentRedirect } from "next/navigation";
-import React from "react";
+import HeaderBgChanger from "@/components/HeaderBgChanger";
+import PagePadding from "@/components/PagePadding";
+import PlaylistCarousel from "@/components/PlaylistCarousel";
+import SongCardExpand from "@/components/SongCardExpand";
+import WhiteButton from "@/components/elements/WhiteButton";
+import BlackButton from "@/components/elements/BlackButton";
 
 interface ChannelPageProps {
 	params: {
@@ -21,6 +26,62 @@ const page = async (props: ChannelPageProps) => {
 	return (
 		<PagePadding>
 			<HeaderBgChanger imageSrc={imageSrc} />
+			<div className='mt-[150px]'></div>
+			<section>
+				<div className=' text-[28px] font-bold'>{channel.name}</div>
+				<article className='mt-4 lg:hidden'>
+					<div>
+						<BlackButton
+							className={"w-[230px] flex justify-center"}
+							label={"구독중 4.18만"}
+						/>
+					</div>
+					<div className='flex flex-row gap-4 mt-4'>
+						<WhiteButton
+							label={"셔플"}
+							icon={<FiShuffle size={16}></FiShuffle>}
+						/>
+						<WhiteButton
+							label={"뮤직 스테이션"}
+							icon={<FiMusic size={16} />}
+						/>
+					</div>
+				</article>
+				<div className='hidden lg:flex flex-row items-center gap-4 text-[14px] mt-4'>
+					<WhiteButton
+						label={"셔플"}
+						icon={<FiShuffle size={16}></FiShuffle>}
+					/>
+					<WhiteButton
+						label={"뮤직 스테이션"}
+						icon={<FiMusic size={16} />}
+					/>
+					<BlackButton
+						className={"w-[230px] flex justify-center"}
+						label={"구독중 4.18만"}
+					/>
+				</div>
+			</section>
+			<section className='mt-[80px]'>
+				<div className=' text-[28px] font-bold'>Song</div>
+				<div className='mt-[20px]'>
+					<ul className='flex flex-col gap-2'>
+						{channel.songList.map((song, key) => {
+							return (
+								<SongCardExpand
+									song={song}
+									key={key}
+								/>
+							);
+						})}
+					</ul>
+				</div>
+			</section>
+			<section className='mt-[80px]'>
+				<div className='text-[28px] font-bold'>Album</div>
+				<PlaylistCarousel playlistArray={channel.playlistArray} />
+			</section>
+			<section className='mt-[80px]'></section>
 		</PagePadding>
 	);
 };
