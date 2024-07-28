f67835f2a07bd6ca923c7d2fc2d1d36691407b14
garamflow <garamflow@gmail.com>
2024-07-28 22:38:23 +0900

    feat: 플레이어 state 관련 타입 지정 및 상태 업데이트 메소드 구현

--- hooks/usePlayerState.ts
+++ hooks/usePlayerState.ts
@@ -1,13 +1,60 @@
+import { Song } from "@/types";
 import { create } from "zustand";
 
 interface PlayerState {
-	isVisiblePlayer: boolean;
-	setIsVisiblePlayer: (isVisiblePlayer: boolean) => void;
+	isPlayerVisible: boolean;
+	setPlayerVisible: (isPlayerVisible: boolean) => void;
+	activeSong?: Song | null;
+	previousQueue: Song[];
+	nextQueue: Song[];
+	setSongQueue: (songList: Song[]) => void;
+	playNext: () => void;
+	playBack: () => void;
 }
 
 const usePlayerState = create<PlayerState>((set) => ({
-	isVisiblePlayer: false,
-	setIsVisiblePlayer: (isVisiblePlayer: boolean) => set({ isVisiblePlayer }),
+	isPlayerVisible: false,
+	setPlayerVisible: (isPlayerVisible: boolean) => set({ isPlayerVisible }),
+	activeSong: null,
+	previousQueue: [],
+	nextQueue: [],
+	setSongQueue: (songList: Song[]) =>
+		set((prev) => {
+			const prevSong = prev.activeSong;
+			const cloneSongList = [...songList];
+			const currentSong = cloneSongList.splice(0, 1)?.[0];
+
+			return {
+				activeSong: currentSong,
+				previousQueue: prevSong ? [prevSong, ...prev.previousQueue] : prev.previousQueue,
+				nextQueue: [...cloneSongList],
+				isVisiblePlayer: true,
+			};
+		}),
+	playNext: () =>
+		set((prev) => {
+			const currentSong = prev.activeSong;
+			const nextQueue = [...prev.nextQueue];
+			const nextSong = nextQueue.shift();
+
+			return {
+				activeSong: nextSong || null,
+				nextQueue: nextQueue,
+				previousQueue: currentSong ? [currentSong, ...prev.previousQueue] : prev.previousQueue,
+			};
+		}),
+	playBack: () =>
+		set((prev) => {
+			const currentSong = prev.activeSong;
+			const previousQueue = [...prev.previousQueue];
+			const prevSong = previousQueue.shift();
+
+			return {
+				activeSong: prevSong || null,
+				nextQueue: currentSong ? [currentSong, ...prev.nextQueue] : prev.previousQueue,
+				previousQueue: previousQueue,
+			};
+		}),
 }));
 
 export default usePlayerState;
