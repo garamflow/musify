67c218f4aae94780d2bf243d192a49e381015aa4
garamflow <garamflow@gmail.com>
2024-07-29 02:10:01 +0900

    feat: 플레이어 연결을 위해 컴포넌트 분리 및 플레이어 연결 구현

--- app/channel/[id]/page.tsx
+++ app/channel/[id]/page.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { permanentRedirect } from "next/navigation";
-import { FiMusic, FiShuffle } from "react-icons/fi";
 import { getChannelById } from "@/lib/dummyData";
 import { getRandomElementFromArray } from "@/lib/utils";
 import HeaderBgChanger from "@/components/HeaderBgChanger";
 import PagePadding from "@/components/PagePadding";
 import PlaylistCarousel from "@/components/PlaylistCarousel";
 import SongCardExpand from "@/components/SongCardExpand";
-import WhiteButton from "@/components/elements/WhiteButton";
-import BlackButton from "@/components/elements/BlackButton";
+import ChannelHead from "@/app/channel/components/ChannelHead";
 
 interface ChannelPageProps {
 	params: {
@@ -27,41 +25,7 @@ const page = async (props: ChannelPageProps) => {
 		<PagePadding>
 			<HeaderBgChanger imageSrc={imageSrc} />
 			<div className='mt-[150px]'></div>
-			<section>
-				<div className=' text-[28px] font-bold'>{channel.name}</div>
-				<article className='mt-4 lg:hidden'>
-					<div>
-						<BlackButton
-							className={"w-[230px] flex justify-center"}
-							label={"구독중 4.18만"}
-						/>
-					</div>
-					<div className='flex flex-row gap-4 mt-4'>
-						<WhiteButton
-							label={"셔플"}
-							icon={<FiShuffle size={16}></FiShuffle>}
-						/>
-						<WhiteButton
-							label={"뮤직 스테이션"}
-							icon={<FiMusic size={16} />}
-						/>
-					</div>
-				</article>
-				<div className='hidden lg:flex flex-row items-center gap-4 text-[14px] mt-4'>
-					<WhiteButton
-						label={"셔플"}
-						icon={<FiShuffle size={16}></FiShuffle>}
-					/>
-					<WhiteButton
-						label={"뮤직 스테이션"}
-						icon={<FiMusic size={16} />}
-					/>
-					<BlackButton
-						className={"w-[230px] flex justify-center"}
-						label={"구독중 4.18만"}
-					/>
-				</div>
-			</section>
+			<ChannelHead channel={channel} />
 			<section className='mt-[80px]'>
 				<div className=' text-[28px] font-bold'>Song</div>
 				<div className='mt-[20px]'>
